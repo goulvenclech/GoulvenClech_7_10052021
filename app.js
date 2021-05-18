16eb3193604fbf05bff7fe5e84923542828488a0
Goulven Nev <nevilain@gmail.com>
2021-05-18 19:13:32 +0200

    tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import dependencies
 import "tailwindcss/tailwind.css"
+import data from "./assets/data/data.json"
 
 document.body.insertAdjacentHTML('afterbegin', `
   <div class="m-12">
@@ -7,5 +8,20 @@ document.body.insertAdjacentHTML('afterbegin', `
       Les Petits Plats
     </h1>
     <p>Ca va faire de l'algo ici...</p>
+    <input type="text" class="my-4 border border-gray-500 p-2 border-1 rounded-sm">
+    </input>
+    <ul>
+    </ul>
   </div>
 `);
+
+document.querySelector("input").addEventListener('input', function(event) {
+  document.querySelectorAll("li").forEach(element => {element.remove()});;
+    data.recipes.forEach(recipe => {
+      if (recipe.name.toLowerCase().includes(event.target.value.toLowerCase())) {
+        document.querySelector("ul").insertAdjacentHTML('afterBegin', `
+          <li class="list-disc ml-5">`+ recipe.name  +`</li>
+        `)
+      }
+    })
+})
